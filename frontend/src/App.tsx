128cfc018323e3942394e5fdd36007dcc3a3e8af
Pedro Nery <pedrohnery@gmail.com>
2022-03-12 15:19:33 -0300

    Navigation buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App(){
       <Navbar />
       <Routes>
 
-        <Route path="/list" element={<Listing />} />
+        <Route path="/" element={<Listing />} />
         
         <Route path="/form">
           <Route path=":movieId" element={<Form/>} />
